325e22e0f470f1888fd1139f88cc9fcce76245d2
Chris Gibson <chrisjo1329@gmail.com>
2026-03-18 15:10:10 +0000

    Next attempt to fix question mark issue

--- module/project_driver.c
+++ module/project_driver.c
@@ -1,6 +1,7 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/init.h>
+#include <linux/input-event-codes.h>
 #include <linux/input.h>
 #include <linux/ioctl.h>
 #include <linux/kernel.h>
@@ -97,53 +98,20 @@ static struct usb_device* usb_dev = NULL; // usb_device struct. if we have this,
 static struct file* usb_fp = NULL; // file pointer for the terrible solution
 
 static const char keycode_map[KEY_MAX] = {
-    [KEY_A] = 'a',
-    [KEY_B] = 'b',
-    [KEY_C] = 'c',
-    [KEY_D] = 'd',
-    [KEY_E] = 'e',
-    [KEY_F] = 'f',
-    [KEY_G] = 'g',
-    [KEY_H] = 'h',
-    [KEY_I] = 'i',
-    [KEY_J] = 'j',
-    [KEY_K] = 'k',
-    [KEY_L] = 'l',
-    [KEY_M] = 'm',
-    [KEY_N] = 'n',
-    [KEY_O] = 'o',
-    [KEY_P] = 'p',
-    [KEY_Q] = 'q',
-    [KEY_R] = 'r',
-    [KEY_S] = 's',
-    [KEY_T] = 't',
-    [KEY_U] = 'u',
-    [KEY_V] = 'v',
-    [KEY_W] = 'w',
-    [KEY_X] = 'x',
-    [KEY_Y] = 'y',
-    [KEY_Z] = 'z',
-    [KEY_1] = '1',
-    [KEY_2] = '2',
-    [KEY_3] = '3',
-    [KEY_4] = '4',
-    [KEY_5] = '5',
-    [KEY_6] = '6',
-    [KEY_7] = '7',
-    [KEY_8] = '8',
-    [KEY_9] = '9',
-    [KEY_0] = '0',
-    [KEY_SPACE] = ' ',
-    [KEY_DOT] = '.',
-    [KEY_COMMA] = ',',
-    [KEY_APOSTROPHE] = '\'',
-    [KEY_SEMICOLON] = ';'[KEY_SLASH] = '/',
+    [KEY_A] = 'a',     [KEY_B] = 'b',   [KEY_C] = 'c',     [KEY_D] = 'd',           [KEY_E] = 'e',         [KEY_F] = 'f',     [KEY_G] = 'g',       [KEY_H] = 'h', [KEY_I] = 'i',
+    [KEY_J] = 'j',     [KEY_K] = 'k',   [KEY_L] = 'l',     [KEY_M] = 'm',           [KEY_N] = 'n',         [KEY_O] = 'o',     [KEY_P] = 'p',       [KEY_Q] = 'q', [KEY_R] = 'r',
+    [KEY_S] = 's',     [KEY_T] = 't',   [KEY_U] = 'u',     [KEY_V] = 'v',           [KEY_W] = 'w',         [KEY_X] = 'x',     [KEY_Y] = 'y',       [KEY_Z] = 'z', [KEY_1] = '1',
+    [KEY_2] = '2',     [KEY_3] = '3',   [KEY_4] = '4',     [KEY_5] = '5',           [KEY_6] = '6',         [KEY_7] = '7',     [KEY_8] = '8',       [KEY_9] = '9', [KEY_0] = '0',
+    [KEY_SPACE] = ' ', [KEY_DOT] = '.', [KEY_COMMA] = ',', [KEY_APOSTROPHE] = '\'', [KEY_SEMICOLON] = ';', [KEY_SLASH] = '/', [KEY_KPSLASH] = '/',
+
 };
 static const char keycode_map_shift[KEY_MAX] = {
-    [KEY_A] = 'A', [KEY_B] = 'B', [KEY_C] = 'C', [KEY_D] = 'D',     [KEY_E] = 'E',   [KEY_F] = 'F',     [KEY_G] = 'G',          [KEY_H] = 'H',         [KEY_I] = 'I',    [KEY_J] = 'J', [KEY_K] = 'K',
-    [KEY_L] = 'L', [KEY_M] = 'M', [KEY_N] = 'N', [KEY_O] = 'O',     [KEY_P] = 'P',   [KEY_Q] = 'Q',     [KEY_R] = 'R',          [KEY_S] = 'S',         [KEY_T] = 'T',    [KEY_U] = 'U', [KEY_V] = 'V',
-    [KEY_W] = 'W', [KEY_X] = 'X', [KEY_Y] = 'Y', [KEY_Z] = 'Z',     [KEY_1] = '!',   [KEY_2] = '@',     [KEY_3] = '#',          [KEY_4] = '$',         [KEY_5] = '%',    [KEY_6] = '^', [KEY_7] = '&',
-    [KEY_8] = '*', [KEY_9] = '(', [KEY_0] = ')', [KEY_SPACE] = ' ', [KEY_DOT] = '>', [KEY_COMMA] = '<', [KEY_APOSTROPHE] = '"', [KEY_SEMICOLON] = ':', [KEY_SLASH] = '?'};
+    [KEY_A] = 'A',     [KEY_B] = 'B',   [KEY_C] = 'C',     [KEY_D] = 'D',          [KEY_E] = 'E',         [KEY_F] = 'F',     [KEY_G] = 'G',       [KEY_H] = 'H', [KEY_I] = 'I',
+    [KEY_J] = 'J',     [KEY_K] = 'K',   [KEY_L] = 'L',     [KEY_M] = 'M',          [KEY_N] = 'N',         [KEY_O] = 'O',     [KEY_P] = 'P',       [KEY_Q] = 'Q', [KEY_R] = 'R',
+    [KEY_S] = 'S',     [KEY_T] = 'T',   [KEY_U] = 'U',     [KEY_V] = 'V',          [KEY_W] = 'W',         [KEY_X] = 'X',     [KEY_Y] = 'Y',       [KEY_Z] = 'Z', [KEY_1] = '!',
+    [KEY_2] = '@',     [KEY_3] = '#',   [KEY_4] = '$',     [KEY_5] = '%',          [KEY_6] = '^',         [KEY_7] = '&',     [KEY_8] = '*',       [KEY_9] = '(', [KEY_0] = ')',
+    [KEY_SPACE] = ' ', [KEY_DOT] = '>', [KEY_COMMA] = '<', [KEY_APOSTROPHE] = '"', [KEY_SEMICOLON] = ':', [KEY_SLASH] = '?', [KEY_KPSLASH] = '/',
+};
 
 
 // write to the actual device. should only ever receive 3 bytes but i'm being safe
