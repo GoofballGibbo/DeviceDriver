3e05e89be55d0aa867484c91b228e7125e661b0f
Chris Gibson <chrisjo1329@gmail.com>
2026-03-18 14:25:57 +0000

    Corrected devfile path

--- wpm_common.h
+++ wpm_common.h
@@ -7,18 +7,17 @@
 #include <sys/ioctl.h>
 
 typedef struct {
-  int index;
-  char ch;
+    int index;
+    char ch;
 } expected_char_t;
 typedef struct {
-  int index;
-  char expected;
-  char typed;
-  int correct;
+    int index;
+    char expected;
+    char typed;
+    int correct;
 } keystroke_result_t;
 typedef struct {
-  int correct_words, missed_words, correct_chars, missed_chars, elapsed_seconds,
-      wpm, raw_wpm;
+    int correct_words, missed_words, correct_chars, missed_chars, elapsed_seconds, wpm, raw_wpm;
 } wpm_stats_t;
 
 #define WPM_MAGIC 'W'
@@ -27,6 +26,6 @@ typedef struct {
 #define WPM_RESET _IO(WPM_MAGIC, 2)
 #define WPM_GET_STATS _IOR(WPM_MAGIC, 3, wpm_stats_t)
 #define WPM_SET_LED _IOW(WPM_MAGIC, 4, int)
-#define DEVICE "/dev/wpm_driver"
+#define DEVICE "/dev/uniprojdev"
 
 #endif
